e3d33d6bb60839b7cf91a73e8bcb412b5ef1eb92
lajkak <92091234+LajkaK2006@users.noreply.github.com>
2025-03-13 13:31:05 +0100

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,4 @@
+// Például: Animáció sebességének változtatása JavaScript-tel
+document.querySelector('.mini-car').addEventListener('click', function() {
+    this.style.animationDuration = '5s'; // Ha rákattintunk, gyorsabban fog mozogni
+});
